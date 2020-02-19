6f92e02710ed6bd620bde76f01b5318fc209897f
Pratik <pr.tikgohil@gmail.com>
2020-02-19 10:58:35 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,6 +10,6 @@ end
 def input_to_index input
   input.to_i - 1
 end
-def move (board, index, character = 'X')
-  board[index] = character
+def move (board, index, char = 'X')
+  board[index] = char
 end
